815ca9c5730f170cbe22eeff820647d5278699c0
kaihokori <96321538+kaihokori@users.noreply.github.com>
2025-03-22 03:59:54 +1100

    Initial Commit

---
+++ UniPass/UniPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniPassApp.swift
+//  UniPass
+//
+//  Created by Kyle Graham on 22/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct UniPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
