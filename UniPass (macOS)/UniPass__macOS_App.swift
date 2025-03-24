390e7e499a952e839027afb643aefa2d2b19007b
kaihokori <96321538+kaihokori@users.noreply.github.com>
2025-03-24 19:25:55 +1100

    Updating onboarding images with transparency

---
+++ UniPass (macOS)/UniPass__macOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  UniPass__macOS_App.swift
+//  UniPass (macOS)
+//
+//  Created by Kyle Graham on 24/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct UniPass__macOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
